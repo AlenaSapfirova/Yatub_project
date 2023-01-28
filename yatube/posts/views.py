8501f5ded0e920ff1198a2cc1b7bd932861b790f
Alena Sapfirova <sapfirova.alena@yandex.ru>
2023-01-28 19:11:32 +0300

    finaly files

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,24 +1,21 @@
-from django.shortcuts import render, get_object_or_404
-
-
-from django.http import HttpResponse
-from .models import Post
-from .models import Group
-
-def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
-    context = {
-        'posts':posts,
-    }
-    return render(request, 'posts/index.html', context)
+from django.shortcuts import get_object_or_404, render
+# from django.http import HttpResponse
+from .models import Group, Post
 
 
 def group_posts(requets, slug):
-    group = get_object_or_404(Group, slug = slug)
+    group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
         'posts': posts,
-    }
+              }
     return render(requets, 'posts/group_list.html', context)
 
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
